9d256211039ee7e4275f17ce9623da1a9d4f32fa
Damian Thomas <damian@falchionstudios.com>
2026-02-12 16:01:42 -0500

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import { defineConfig } from "astro/config";
 // TODO: add include() list for icons to avoid bundle bloat
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.NODE_ENV
     ? "http://localhost:3009"
     : "https://prismfl.org",
+
   integrations: [icon()],
   output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
