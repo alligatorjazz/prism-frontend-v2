12e1c5f61af38de9a4f7929b261d55fd39a71980
Damian Thomas <ajazz@MacBookAir.lan>
2026-04-10 11:07:41 -0400

    add rudimentary section border support;

--- src/types/sections.ts
+++ src/types/sections.ts
@@ -1,12 +1,3 @@
-export type RichText = { content: string };
-
-export type ColumnChild = RichText;
-export type Column = {
-  hideOnMobile?: boolean;
-  hideOnDesktop?: boolean;
-  // content: ColumnChild;
-};
-
 export type SectionProps = {
   title: string;
   fullWidth?: boolean;
