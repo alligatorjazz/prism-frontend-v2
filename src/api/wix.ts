99b966a6202d162474f099b77810d341335b4f33
ajazz <ajazz@fromthesuperhighway.com>
2026-06-16 12:03:16 -0400

    feat: loading events from wix

--- src/api/wix.ts
+++ src/api/wix.ts
@@ -1,36 +1,63 @@
 import { createClient, ApiKeyStrategy } from "@wix/sdk";
 import { items } from "@wix/data";
-import { writeFileSync } from "node:fs";
+import { wixEventsV2 } from "@wix/events";
 
 const wix = createClient({
   auth: ApiKeyStrategy({
     apiKey: import.meta.env.WIX_API_KEY,
     siteId: import.meta.env.WIX_SITE_ID,
   }),
-  modules: { items },
+  modules: { items, wixEventsV2 },
 });
 
 export async function getItems(collection: string) {
-  console.debug("getting items...");
   const collectionQuery = await wix.items.query(collection).find();
   let hasNext = collectionQuery.hasNext();
   let items = collectionQuery.items;
 
-  console.debug("items : ", items);
   while (hasNext) {
-    console.debug("fetching next page...");
     const nextQuery = await collectionQuery.next();
     items = [...items, ...nextQuery.items];
-    console.debug("items : ", items);
-
     hasNext = nextQuery.hasNext();
   }
 
-  console.debug(collection);
   return items;
 }
 
-writeFileSync(
-  "partners.json",
-  JSON.stringify(await getItems("Partners"), null, 4),
-);
+export async function getEvents() {
+  const allEvents: any[] = [];
+  const limit = 100;
+  let cursor: string | null = null;
+  let hasMore = true;
+
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+
+  while (hasMore) {
+    const response: any = await wix.wixEventsV2.queryEvents({
+      filter: {
+        status: { $in: ["UPCOMING"] },
+        "dateAndTimeSettings.startDate": {
+          $gte: oneYearAgo,
+        },
+      },
+      sort: [{ fieldName: "dateAndTimeSettings.startDate", order: "ASC" }],
+      paging: {
+        limit,
+        ...(cursor ? { cursor } : {}),
+      },
+    });
+
+    allEvents.push(...response.events);
+
+    if (response.pagingMetadata?.cursors?.next) {
+      cursor = response.pagingMetadata.cursors.next;
+    } else {
+      hasMore = false;
+    }
+  }
+
+  console.log(allEvents);
+
+  return allEvents;
+}
