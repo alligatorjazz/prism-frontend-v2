25b9c7b0abd13c2a09d0da32f84914c5e59fc2c8
ajazz <ajazz@fromthesuperhighway.com>
2026-08-18 13:01:34 -0400

    fix border imagel oading error

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: "https://staging.prismfl.org",
   server: {
     port: 3000,
-		host: "0.0.0.0"
+    host: "0.0.0.0",
   },
   integrations: [icon(), react()],
   output: "server",
